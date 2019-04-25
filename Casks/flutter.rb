ea6938ad9b5621eb4958707c5660dd1983df667e
akassharjun <38319057+akassharjun@users.noreply.github.com>
2019-04-25 19:03:49 +0530

    Updated script for Flutter 1.2.1

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -1,5 +1,5 @@
 cask 'flutter' do
-  version "1.0.0"
+  version "1.2.1"
   sha256 "0b4cacbe84fd68cc9cfe213a90ddcdba558e45c19be30c6173c07a318be34014"
 
   url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v#{version}-stable.zip"
